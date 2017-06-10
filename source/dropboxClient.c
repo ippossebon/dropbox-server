8bd0b76c2fb9d68b0644f31f98a9721facc24f19
Isadora <isadorapossebon@gmail.com>
2017-06-10 12:25:46 -0300

    Identifica modificacao de arquivos no client, cria pasta stnc_dir e adiciona gitignore

--- source/dropboxClient.c
+++ source/dropboxClient.c
@@ -64,9 +64,7 @@ void close_connection(){
 /* Sincroniza o diretório “sync_dir_<nomeusuário>” com
 o servidor */
 void sync_client(){
-
-
-    /* Aqui estará a nova lista com os arquivos atuais/reais 
+    /* Aqui estará a nova lista com os arquivos atuais/reais
         Vão em frente, troquem o nome... */
     real_current_files = fn_create_from_path(sync_dir);
 
@@ -78,13 +76,13 @@ void sync_client(){
     for (node = real_current_files; node !=NULL; node = node->next) {
         /* Retorna o file_info do nodo se encontrado, se não retorna NULL */
         file_info* old_file = fn_find(current_files, node->data->name);
-        
+
         if(old_file != NULL){ //encontrou o arquivo, verifica se foi modificado
             /* s1 < s2 = numero negativo, entao s2 foi atualizada */
             if(strcmp(old_file->last_modified, node->data->last_modified) < 0){
                 printf("O arquivo %s foi MODIFICADO às %s\n", node->data->name, node->data->last_modified);
             }
-        }else{ //Não encontrou o arquivo, então ele foi adicionado 
+        }else{ //Não encontrou o arquivo, então ele foi adicionado
             printf("O arquivo %s foi ADICIONADO às %s\n", node->data->name, node->data->last_modified);
         }
 
@@ -97,17 +95,17 @@ void sync_client(){
     for (node = current_files; node !=NULL; node = node->next) {
         /* Retorna o file_info do nodo se encontrado, se não retorna NULL */
         file_info* old_file = fn_find(real_current_files, node->data->name);
-        
+
         if(old_file != NULL){ //encontrou o arquivo, verifica se foi modificado
             /* s1 < s2 = numero negativo, entao s2 foi atualizada */
             if(strcmp(old_file->last_modified, node->data->last_modified) < 0){
-                printf("O arquivo %s foi MODIFICADO às %s\n", node->data->name, node->data->last_modified);          
+                printf("O arquivo %s foi MODIFICADO às %s\n", node->data->name, node->data->last_modified);
             }
         }else{ //Não encontrou o arquivo, então ele foi deletado
             printf("O arquivo %s foi DELETADO.\n", node->data->name);
         }
-        
-       
+
+
     }
 
     /* current_file agora aponta para a real_current_files */
